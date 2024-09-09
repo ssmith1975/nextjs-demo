44e34d540ec0554d2eda75a333cd834d61d91a03
ssmith1975 <ssmith1975.soc@gmail.com>
2024-09-09 06:48:39 -0500

    Fix auth error for prod

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import { authConfig } from './app/api/auth/[...nextauth]/auth.config'; //'./auth.config';
 
 export default NextAuth(authConfig).auth;
 
